fad6543e85113e5765999f6c3aca3d0919b6d94c
Phillip Gomez <gomez.phillip@gmail.com>
2012-05-14 20:26:41 -0700

    Removed ssh code since it is no longer used

--- web_client/control.php
+++ web_client/control.php
@@ -13,36 +13,6 @@ function directv_control($command)
 	return $output;
 }
 
-function directv_control_ssh($command)
-{
-   if (!function_exists('ssh2_auth_pubkey_file'))
-   {
-      return FALSE;
-   }
-
-   if ($con = ssh2_connect("philgomez.com", 2154, array('hostkey'=>'ssh-rsa'))) 
-   {
-      if (ssh2_auth_pubkey_file($con, 'mediacenter', '/home/phillip/Documents/id_rsa.pub', '/home/phillip/Documents/id_rsa'))
-      {
-            // execute a command
-            if ($stream = ssh2_exec($con, '/home/mediacenter/src/Directv_Control/directv.pl '.$command))
-            {
-               // collect returning data from command
-               stream_set_blocking($stream, true);
-               $data = "";
-               while ($buf = fread($stream,4096)) {
-                  $data .= $buf;
-               }
-               fclose($stream);
-               return $data;
-         }
-      }
-   }
-
-   return FALSE;
-}
-
-
 if (isset($_GET['reset']))
 {
 	reset_stream();
