3da283eb085fec8cb25daec35f83f2973c4d7172
Phillip Gomez <gomez.phillip@gmail.com>
2012-05-14 20:32:29 -0700

    Updated with initial weblcient code and directv script

--- web_client/control.php
+++ web_client/control.php
@@ -1,28 +1,29 @@
 <?php 
 
-$STREAMING_SCRIPT_DIR = "/home/gomez/src/hdpvr-tools/streaming_scripts"
+$SCRIPT_DIR = "/home/gomez/src/hdpvr-tools/scripts"
 
 function reset_stream()
 {
-	$output = shell_exec("cd $STREAMING_SCRIPT_DIR; ./restart.sh &>/dev/null");
+	$output = shell_exec("cd $SCRIPT_DIR; ./restart.sh &>/dev/null");
 }
 
 function directv_control($command)
 {
-	$output = shell_exec('/home/gomez/src/DirectTV/directv.pl '.$command);
+	$output = shell_exec("$SCRIPT_DIR/directv.pl ".$command);
 	return $output;
 }
 
 if (isset($_GET['reset']))
 {
-	reset_stream();
+   reset_stream();
+   $data = array('response' => 'success');
+   echo json_encode($data);
 }
 
 if (isset($_GET['command']))
 {
   $response = directv_control($_GET['command']);
   $parts = preg_split("/ /", $response);
-
 
    $channel = trim($parts[1]);
    if ($channel == null)
